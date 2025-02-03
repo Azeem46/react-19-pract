a66c5d88b575a15f539a26f5f6812612dc25e864
Azeem46 <shaikhazeem4646@gmail.com>
2025-02-03 22:29:16 +0530

    implemented feature for trending movies

--- react-19-pract/src/App.jsx
+++ react-19-pract/src/App.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import Spinner from "./components/Spinner";
 import MovieCard from "./components/MovieCard";
 import { useDebounce } from "react-use";
+import { updateSearchCount } from "./appwrite";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -45,6 +46,9 @@ const App = () => {
         return;
       }
       setMovieList(data.results || []);
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
     } catch (error) {
       console.error(`Error fetching movies: ${error}`);
       setErrorMessage("Error Fetching movies,Please try again");
